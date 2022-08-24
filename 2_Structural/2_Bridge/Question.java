cb32608d67c8b5f78d412d82df3bcdce25b536b4
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-24 20:49:38 +0800

    Bridge Pattern added.

---
+++ 2_Structural/2_Bridge/Question.java
@@ -0,0 +1,11 @@
+// this is the Question interface.  
+public interface Question {
+    
+    public void nextQuestion();  
+    public void previousQuestion();  
+    public void newQuestion(String q);  
+    public void deleteQuestion(String q);  
+    public void displayQuestion();  
+    public void displayAllQuestions();  
+
+} // End of the Question interface.  
---
+++ 2_Structural/2_Bridge/QuestionFormat.java
@@ -0,0 +1,14 @@
+// this is the QuestionFormat class.  
+public class QuestionFormat extends QuestionManager {
+    
+    public QuestionFormat(String catalog){  
+        super(catalog);  
+    }
+    
+    public void displayAll() {  
+        System.out.println("\n---------------------------------------------------------");  
+        super.displayAll();  
+        System.out.println("-----------------------------------------------------------");  
+    }
+    
+}// End of the QuestionFormat class.  
