3f7c37d86193d31f236aeb82d1a1d85ea5df0f4a
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-24 21:42:09 +0800

    Proxy Pattern added.

---
+++ 2_Structural/7_Proxy/OfficeInternetAccess.java
@@ -0,0 +1,5 @@
+public interface OfficeInternetAccess {
+    
+    public void grantInternetAccess();
+    
+} // 
---
+++ 2_Structural/7_Proxy/ProxyPatternClient.java
@@ -0,0 +1,8 @@
+public class ProxyPatternClient {
+    
+    public static void main(String[] args) {  
+        OfficeInternetAccess access = new ProxyInternetAccess("Ashwani Rajput");
+        access.grantInternetAccess();  
+    }
+    
+} // 
