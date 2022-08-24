9eccdb0f1b7f9238033f42be81d92250a6ae41ed
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-24 23:22:04 +0800

    State Pattern added.

---
+++ 3_Behavioral/8_State/Accounting.java
@@ -0,0 +1,23 @@
+public class Accounting implements Connection {  
+      
+    @Override  
+    public void open() {  
+	System.out.println("open database for accounting");  
+    }
+    
+    @Override  
+    public void close() {  
+	System.out.println("close the database");  
+    }  
+         
+    @Override  
+    public void log() {  
+	System.out.println("log activities");  
+    }  
+         
+    @Override  
+    public void update() {  
+	System.out.println("Accounting has been updated");  
+    }
+    
+}// End of the Accounting class.
