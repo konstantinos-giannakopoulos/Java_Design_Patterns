c831b54ee5d2483c04fa38d680c1ca4bf640fc11
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-24 21:15:57 +0800

    Composite Pattern added.

---
+++ 2_Structural/3_Composite/Employee.java
@@ -0,0 +1,13 @@
+// this is the Employee interface i.e. Component.
+
+public interface Employee {
+    
+    public  int getId();  
+    public String getName();  
+    public double getSalary();  
+    public void print();  
+    public void add(Employee employee);  
+    public void remove(Employee employee);  
+    public Employee getChild(int i);
+    
+}// End of the Employee interface.  
