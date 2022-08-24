e8afe7444e6c249023247e728b2f8c5e3738e11f
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-24 22:34:08 +0800

    Iterator Pattern added.

---
+++ 3_Behavioral/4_Iterator/CollectionofNames.java
@@ -0,0 +1,30 @@
+public class CollectionofNames implements Container {
+    
+    public String name[]={"Ashwani Rajput", "Soono Jaiswal","Rishi Kumar","Rahul Mehta","Hemant Mishra"};   
+    
+    @Override  
+    public Iterator getIterator() {  
+        return new CollectionofNamesIterate() ;  
+    }
+    
+    private class CollectionofNamesIterate implements Iterator{   
+        int i;  
+
+	@Override  
+        public boolean hasNext() {  
+            if (i<name.length) {  
+                return true;  
+            }  
+            return false;  
+        }
+	
+        @Override  
+        public Object next() {  
+            if(this.hasNext()) {  
+                return name[i++];  
+            }  
+            return null;      
+        }  
+    }
+    
+} //
---
+++ 3_Behavioral/4_Iterator/Container.java
@@ -0,0 +1,5 @@
+public interface Container {
+    
+    public Iterator getIterator();
+    
+}// End of the Iterator interface.  
