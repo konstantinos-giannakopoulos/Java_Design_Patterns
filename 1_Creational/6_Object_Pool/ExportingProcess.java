d270a38680786a11af883df54c422c3c30c41616
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-24 20:20:15 +0800

    Object Pool Pattern added.

---
+++ 1_Creational/6_Object_Pool/ExportingProcess.java
@@ -0,0 +1,15 @@
+public class ExportingProcess {
+    
+    private long processNo;  
+    
+    public ExportingProcess(long processNo)  {  
+	this.processNo = processNo;  
+        // do some  expensive calls / tasks here in future  
+	System.out.println("Object with process no. " + processNo + " was created");  
+    }  
+    
+    public long getProcessNo() {  
+        return processNo;  
+    }
+    
+}// End of the ExportingProcess class.  
